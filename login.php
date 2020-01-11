f2ba1c3da76b01fa4c36dbe9c7bab47cc333e48c
amadrafi <auliaahmadrafi@gmail.com>
2020-01-11 23:21:35 +0800

    login and register

---
+++ login.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style.css" type="text/css">
+    <link rel="stylesheet" href="css/login.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Playfair+Display:900|Quicksand:400,700" rel="stylesheet">
+  </head>
+  <body>
+    <div class="navbar">
+      <div class="navbar-img">
+        <a href="#home"><img src="img/navbar-logo.gif" alt="" id="navbar-logo"></a>
+        <h1 class="navbar-title">KTJ SHOP</h1>
+      </div>
+      <div class="search-bar">
+        <ul id="navbar-ul">
+          <li class="nav-item"><a href="index.php" class="nav-link">Homepage</a></li>
+        </ul>
+      </div>
+    </div>
+    <div class="heading">
+      <h1>Login</h1>
+    </div>
+  </body>
+</html>
